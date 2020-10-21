1b66db1e7243f87722b1027660250a18d9a61e73
Anton Månsson <anton.arrod@gmail.com>
2020-10-21 23:41:44 +0200

    completed top artist

--- Application/src/Components/TopArtistsShort.js
+++ Application/src/Components/TopArtistsShort.js
@@ -1,42 +1,75 @@
 import React from 'react';
 import { useStateValue } from "./StateProvider";
 import './TopArtists.scss';
+import ScrollAnimation from 'react-animate-on-scroll';
 
 function TopArtistsShort() {
     const [{ top_artists_short, top_tracks_short }, dispatch] = useStateValue();
 
     return (
-        <div className="topArtists">
-            <h2>Top Artists last month</h2>
-            <div className="artist-details">
+        <ScrollAnimation delay={100}
+            animateIn='fadeInUpBig'
+            initiallyVisible={false}>
 
-                <div className="best-artist">
-                    <div className="image-details">
-                        <img src={top_artists_short?.items[4].images[0].url} className="image-4"></img>
-                        <img src={top_artists_short?.items[3].images[0].url} className="image-3"></img>
-                        <img src={top_artists_short?.items[2].images[0].url} className="image-2"></img>
-                        <img src={top_artists_short?.items[1].images[0].url} className="image-1"></img>
-                        <img src={top_artists_short?.items[0].images[0].url} className="image-0"></img>
+            <div className="topArtists">
+                <ScrollAnimation delay={600}
+                    animateIn='bounceIn'
+                    initiallyVisible={false}>
+                    <h2>Top Artists last month</h2>
+                </ScrollAnimation>
+                <div className="artist-details">
+                    <div className="best-artist">
+                        <div className="image-details">
+                            <ScrollAnimation delay={1600}
+                                animateIn='bounceInLeft'
+                                initiallyVisible={false}>
+                                <img src={top_artists_short?.items[4].images[0].url} className="image-4"></img>
+                            </ScrollAnimation>
+                            <ScrollAnimation delay={1500}
+                                animateIn='bounceInLeft'
+                                initiallyVisible={false}>
+                                <img src={top_artists_short?.items[3].images[0].url} className="image-3"></img>
+                            </ScrollAnimation>
+                            <ScrollAnimation delay={1300}
+                                animateIn='bounceInLeft'
+                                initiallyVisible={false}>
+                                <img src={top_artists_short?.items[2].images[0].url} className="image-2"></img>
+                            </ScrollAnimation>
+                            <ScrollAnimation delay={1100}
+                                animateIn='bounceInLeft'
+                                initiallyVisible={false}>
+                                <img src={top_artists_short?.items[1].images[0].url} className="image-1"></img>
+                            </ScrollAnimation>
+                            <ScrollAnimation delay={900}
+                                animateIn='bounceInLeft'
+                                initiallyVisible={false}>
+                                <img src={top_artists_short?.items[0].images[0].url} className="image-0"></img>
+                            </ScrollAnimation>
+                        </div>
+                    </div>
+                    <div className="Honerable-mentions">
+                        {top_artists_short?.items.map((item) => (
+                            <ArtistList key={item.id} artist={item} />
+                        ))}
                     </div>
                 </div>
-                <div className="Honerable-mentions">
-                    {top_artists_short?.items.map((item) => (
-                        <ArtistList key={item.id} artist={item} />
-                    ))}
-                </div>
             </div>
-        </div>
+        </ScrollAnimation>
     );
 }
 
 
 function ArtistList(artist) {
-    // console.log(artist.artist);
     return (
-        <li className="artist">
+        <ScrollAnimation delay={1200}
+            animateIn='bounceIn'
+            initiallyVisible={false}>
+            <li className="artist">
                 {/* <img src={artist.artist.images[0].url}></img> */}
                 {artist.artist.name}
-        </li>
+            </li>
+        </ScrollAnimation>
+
     );
 }
 export default TopArtistsShort;
